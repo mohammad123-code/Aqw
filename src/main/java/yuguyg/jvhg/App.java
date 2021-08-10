359a3094b31d8c7c46b901db19853ae2d39f638c
Mohammad Anzaf <Mohammad.Anzaf@XEBIAINDIA.LOCAL>
2021-08-10 16:52:53 +0530

    Fristcommit

---
+++ src/main/java/yuguyg/jvhg/App.java
@@ -0,0 +1,13 @@
+package yuguyg.jvhg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
